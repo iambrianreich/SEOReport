53739527bb69072d4c5d5cf67f142b539a3dcea2
Espryon <Espryon@protonmail.ch>
2018-05-17 12:02:47 -0400

    update

--- src/CrawlErrors.php
+++ src/CrawlErrors.php
@@ -1,16 +1,22 @@
 <?php
 
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'GwtCrawlErrors.class.php';
+//require 'GWT_CrawlErrors.class.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'GWT_CrawlErrors.class.php';
 
 /**
  * Example 1:
  * Download a CSV for a specific domain from the browser.
  */
+
 try {
+
     $mail = $_POST["mail"];
     $pass = $_POST["pass"];
-
     $domain = $_POST["domain"];
+	//$mail = 'iheartjavaprogramming@gmail.com';
+	//$pass = 'JamesBondAgent007';
+	//$domain = 'https://thenotaryshop.net/';
+
 
     $gwtCrawlErrors = new GwtCrawlErrors();
 
@@ -23,4 +29,3 @@ catch (Exception $e) {
     die($e->getMessage());
 }
 
-?>
