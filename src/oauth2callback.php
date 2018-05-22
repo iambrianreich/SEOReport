61124ef05c84c9deb3b93da035e317feb40ad105
Espryon <Espryon@protonmail.ch>
2018-05-22 14:18:12 -0400

    backup

--- src/oauth2callback.php
+++ src/oauth2callback.php
@@ -6,7 +6,7 @@ session_start();
 $client = new Google_Client();
 $client->setAuthConfigFile('client_secret_151669692220-f0fe5df5ai6kegeu7q5c56tv6g0enlnp.apps.googleusercontent.com.json');
 $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php');
-$client->addScope(Google_Service_Webmaster);
+$client->addScope('https://www.googleapis.com/auth/webmasters');
 
 if (! isset($_GET['code'])) {
   $auth_url = $client->createAuthUrl();
