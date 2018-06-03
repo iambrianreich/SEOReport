e12be9cc6aeacfcedff84addd9932ea8e6a03433
Andrew O <Espryon@protonmail.ch>
2018-06-03 15:52:04 -0400

    Added basic file handling and passing of URL from $_POST.

--- src/createCSV.php
+++ src/createCSV.php
@@ -35,7 +35,9 @@ if (mysqli_connect_errno())
   echo "Connected successfully <br> <br>";
 }
 
-$sql="SELECT * FROM CrawlError";
+$URL = $_POST['URL'];
+
+$sql="SELECT * FROM CrawlError WHERE url = '$URL'";
 $result=mysqli_query($con,$sql);
 
 //$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
@@ -45,7 +47,12 @@ $result=mysqli_query($con,$sql);
 
 // Fetch all
 $rows=mysqli_fetch_all($result,MYSQLI_ASSOC);
-//var_dump($rows);
+//var_dump($rows); //test statement
+
+if (file_exists("file.csv")) { //if file exists and has write permission, write out to that file.
+
+echo "<br> file exists <br>";
+echo "<br> writing to file <br>";
 
 $fp = fopen('file.csv', 'w');
 
@@ -54,6 +61,38 @@ foreach ($rows as $fields) {
 }
 
 fclose($fp);
+
+}
+
+else { //if file doesn't exist and doesn't have write permission. Create that file and add write permissions.
+
+echo "<br> file doesn't exist, Please note: PHP needs write access to the folder to create the file. <br>";
+echo "<br> attempting to create file & add write permissions <br>";
+
+touch("file.csv");
+chmod("file.csv",0777);
+
+echo "<br> Attempting to write to the file <br> ";
+
+$fp = fopen('file.csv', 'w');
+
+foreach ($rows as $fields) {
+    fputcsv($fp, $fields);
+
+}
+
+fclose($fp);
+
+}
+
+/*
+$fp = fopen('file.csv', 'w');
+
+foreach ($rows as $fields) {
+    fputcsv($fp, $fields);
+}
+
+fclose($fp); */
 
 // Free result set
 //mysqli_free_result($result);
