e869c4e6b2a60246cbe4eb645107a1635e6f2301
Andrew O <Espryon@protonmail.ch>
2018-06-02 22:53:03 -0400

    Added basic ability to list crawl errors from the database.

---
+++ src/listCrawlErrors.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title> SEOReport </title>
+</head>
+<body style="text-align: center";>
+
+<br>
+<br>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "JamesBondAgent007";
+$dbname = "SEO";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+//$sql = "SELECT * from CrawlError WHERE url = $_POST('URL')";
+$sql = "SELECT id, reportID, url, type, errorCount, created, creator, lastModifiedBy, lastModified, platform from CrawlError";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        //echo "<strong> Title: </strong>" . $row["title"]. "<br> <strong> Author: </strong>" . $row["author"]. "<br> <strong> ISBN </strong>" . $row["ISBN"]. " <br> <strong> Genre: </strong>" . $row["Genre"]. "<br> <br>";
+	echo "<strong> id: </strong>" . $row['id'] . "<br> <strong> reportID: </strong>" . $row['reportID'] . "<br> <strong> url: </strong>" . $row['url'] . "<br> <strong> type: </strong>" . $row['type'] . "<br> <strong> errorCount: </strong>" . $row['errorCount'] . "<br> <strong> created: </strong>" . $row['created'] . "<br> <strong> creator: </strong>" . $row['creator'] . "<br> <strong> lastModifiedBy: </strong>" . $row['lastModifiedBy'] . "<br> <strong> lastModified </strong>" . $row['lastModified'] . "<br> <strong> platform: </strong>" . $row['platform'] . "<br> <br>";  
+    }
+
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+
+</body>
+</html>
+
+
